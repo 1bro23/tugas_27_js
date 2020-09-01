eb66e00f4e4e1d007326c3fc157dc73c445cdf05
Ramdhawa <shinlia21@gmail.com>
2020-09-01 17:22:14 +0800

    add tugas 27 js

---
+++ tugas27.js
@@ -0,0 +1,7 @@
+let a="No ",b="rumah saya ",c=30,d="A";
+console.log("Soal 1");
+console.log(a+b+String(c)+d);
+console.log("--------------------------------------".repeat(3));
+let e="Jalan pangeran Tirtayasa No ",f=70;
+console.log("Soal 2");
+console.log(e+String(f));
